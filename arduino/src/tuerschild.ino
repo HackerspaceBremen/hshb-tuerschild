654828468e4b643e70e537d3db350c2005c0abca
Thomas Helmke <thomas.helmke@gmx.de>
2016-03-13 18:44:06 +0100

    Erste auf Hardware getestete Version Einzelne Bereiche können ein- und ausgeschaltet werden. Einfaches Lauflicht funktioniert.
    TODO: Weiches Lauflicht programmieren

--- arduino/src/tuerschild.ino
+++ arduino/src/tuerschild.ino
@@ -1,10 +1,10 @@
 
 #include "Adafruit_NeoPixel.h"
-#include "TimerOne.h"
+/* #include "TimerOne.h" */
 //define the data pin
 #define LEDPIN 10
 //define number of pixels
-#define NUMPIXEL 60
+#define NUMPIXEL 45
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
 // Parameter 3 = pixel type flags, add together as needed:
@@ -17,7 +17,7 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXEL, LEDPIN, NEO_GRB | NEO_KHZ8
 unsigned int i = 0;
 static unsigned long msLast = 0;
 
-#define LEDPROBOX 20
+#define LEDPROBOX 15
 #define BOX1START 0
 #define BOX2START LEDPROBOX
 #define BOX3START LEDPROBOX*2
@@ -31,11 +31,14 @@ static unsigned long msLast = 0;
 
 typedef uint32_t Color;
 Color cBlack = 0;
-Color cWhite = 0x7F7F7F;
+/* Color cWhite = 0x7F7F7F; */
 /* Color cWhite = 0x555555; */
+Color cWhite = 0x3C3C3C;
 Color cRed = 0x7F0000;
 Color cGreen = 0x7F00;
 Color cBlue = 0x7F;
+Color cOrange = 0xEE7600;
+Color cYellow = 0x7F7F00;
 
 float Brightness_1_1 = 0.0;
 float Brightness_1_2 = 0.0;
@@ -44,7 +47,7 @@ float Brightness_2_2 = 0.0;
 float Brightness_3_1 = 0.0;
 float Brightness_3_2 = 0.0;
 
-unsigned long Interval = 20; //Interval für Lauflicht in ms
+unsigned long Interval = 100; //Interval für Lauflicht in ms
 
 void setup ()
 {
@@ -62,53 +65,58 @@ void setup ()
     strip.begin();                  // Initialize NeoPixel object
     strip.show();                   // Initialize all pixels to '}ff'
 
-    msLast = millis();
+    /* msLast = millis(); */
 
 }
 
 void loop ()
 {
     unsigned long ms = millis();
-    if( (ms - msLast) >= Interval)
+    /* Serial.print(BOX2START); */
+    if((ms - msLast) >= Interval)
     {
-        if(digitalRead(BOX1SCHALTER))
+        i++;
+        if(!digitalRead(BOX1SCHALTER))
         {
             digitalWrite(BOX1LED, HIGH);
             LightBox(BOX1START, cWhite);
+            strip.setPixelColor(BOX1START+i%LEDPROBOX,cGreen);
         }
         else
         {
             digitalWrite(BOX1LED, LOW);
             LightBox(BOX1START, cBlack);
         }
-        if(digitalRead(BOX2SCHALTER))
+        if(!digitalRead(BOX2SCHALTER))
         {
             digitalWrite(BOX2LED, HIGH);
             LightBox(BOX2START, cWhite);
+            strip.setPixelColor(BOX2START+LEDPROBOX-1-i%LEDPROBOX,cOrange);
         }
         else
         {
             digitalWrite(BOX2LED, LOW);
             LightBox(BOX2START, cBlack);
         }
-        if(digitalRead(BOX3SCHALTER))
+        if(!digitalRead(BOX3SCHALTER))
         {
             digitalWrite(BOX3LED, HIGH);
             LightBox(BOX3START, cWhite);
+            strip.setPixelColor(BOX3START+i%LEDPROBOX,cYellow);
         }
         else
         {
             digitalWrite(BOX3LED, LOW);
             LightBox(BOX3START, cBlack);
         }
+        msLast = millis();
     }
     strip.show();
-    msLast = millis();
 }
 
 void LightBox(int start, uint32_t col)
 {
-    for(int i = start; i < LEDPROBOX; i++)
+    for(int i = start; i < start + LEDPROBOX; i++)
     {
         strip.setPixelColor(i, col);
     }
