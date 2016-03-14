a128e5444791d0ece6f249d5e7d98780cb76362e
Thomas Helmke <thomas.helmke@gmx.de>
2016-03-14 17:12:42 +0100

    Neue Version mit weicherem Farbverlauf

--- arduino/src/tuerschild.ino
+++ arduino/src/tuerschild.ino
@@ -1,5 +1,6 @@
 
 #include "Adafruit_NeoPixel.h"
+#include "rgb.h"
 /* #include "TimerOne.h" */
 //define the data pin
 #define LEDPIN 10
@@ -15,6 +16,7 @@
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXEL, LEDPIN, NEO_GRB | NEO_KHZ800);
 
 unsigned int i = 0;
+unsigned int j = 0;
 static unsigned long msLast = 0;
 
 #define LEDPROBOX 15
@@ -34,20 +36,70 @@ Color cBlack = 0;
 /* Color cWhite = 0x7F7F7F; */
 /* Color cWhite = 0x555555; */
 Color cWhite = 0x3C3C3C;
-Color cRed = 0x7F0000;
-Color cGreen = 0x7F00;
-Color cBlue = 0x7F;
-Color cOrange = 0xEE7600;
-Color cYellow = 0x7F7F00;
+/* Color cRed = 0x7F0000; */
+/* Color cGreen = 0x7F00; */
+/* Color cBlue = 0x7F; */
+/* Color cOrange = 0xEE7600; */
+/* Color cYellow = 0x7F7F00; */
 
-float Brightness_1_1 = 0.0;
-float Brightness_1_2 = 0.0;
-float Brightness_2_1 = 0.0;
-float Brightness_2_2 = 0.0;
-float Brightness_3_1 = 0.0;
-float Brightness_3_2 = 0.0;
+#define GreenR 0
+#define GreenG 127
+#define GreenB 0
+#define YellowR 127
+#define YellowG 127
+#define YellowB 0
+#define OrangeR 238
+#define OrangeG 118
+#define OrangeB 0
 
-unsigned long Interval = 100; //Interval für Lauflicht in ms
+#define WhiteS 0
+#define WhiteV 24
+
+#define GreenH 120
+#define GreenS 100
+#define GreenV 100
+hsv GreenHsv = {GreenH, GreenS/100.0, GreenV/100.0};
+#define OrangeH 30
+#define OrangeS 100
+#define OrangeV 93
+hsv OrangeHsv = {OrangeH, OrangeS/100.0, OrangeV/100.0};
+#define YellowH 60
+#define YellowS 100
+#define YellowV 50
+hsv YellowHsv = {YellowH, YellowS/100.0, YellowV/100.0};
+
+#define BSTEPS 20
+
+float Brightness[BSTEPS] = {
+    0.05,
+    0.1, 0.15,
+    0.2, 0.25,
+    0.3, 0.35,
+    0.4, 0.45,
+    0.5, 0.55,
+    0.6, 0.65,
+    0.7, 0.75,
+    0.8, 0.85,
+    0.9, 0.95,
+    1.0
+};
+float Brightness2[BSTEPS] = {
+    1.0, 0.95,
+    0.9, 0.85,
+    0.8, 0.75,
+    0.7, 0.65,
+    0.6, 0.55,
+    0.5, 0.45,
+    0.4, 0.35,
+    0.3, 0.25,
+    0.2, 0.15,
+    0.1, 0.05
+};
+
+
+
+unsigned long Interval = 20; //Interval für Lauflicht in ms
+
 
 void setup ()
 {
@@ -75,12 +127,26 @@ void loop ()
     /* Serial.print(BOX2START); */
     if((ms - msLast) >= Interval)
     {
-        i++;
+        j++;
+        if(j%BSTEPS == 0)
+        {
+            i++;
+        }
         if(!digitalRead(BOX1SCHALTER))
         {
             digitalWrite(BOX1LED, HIGH);
             LightBox(BOX1START, cWhite);
-            strip.setPixelColor(BOX1START+i%LEDPROBOX,cGreen);
+            hsv GreenHsv1 = GreenHsv;
+            GreenHsv1.s = GreenHsv.s - Brightness2[j%BSTEPS]*(GreenS-WhiteS)/100.0;
+            GreenHsv1.v = GreenHsv.v - Brightness2[j%BSTEPS]*(GreenV-WhiteV)/100.0;
+            rgb GreenRgb1 = hsv2rgb(GreenHsv1);
+            strip.setPixelColor(BOX1START+i%LEDPROBOX,GreenRgb1.r*255,GreenRgb1.g*255,GreenRgb1.b*255);
+            hsv GreenHsv2 = GreenHsv;
+            GreenHsv2.s = GreenHsv.s - Brightness[j%BSTEPS]*(GreenS-WhiteS)/100.0;
+            GreenHsv2.v = GreenHsv.v - Brightness[j%BSTEPS]*(GreenV-WhiteV)/100.0;
+            rgb GreenRgb2 = hsv2rgb(GreenHsv2);
+            strip.setPixelColor(BOX1START+i%LEDPROBOX-1,GreenRgb2.r*255,GreenRgb2.g*255,GreenRgb2.b*255);
+
         }
         else
         {
@@ -91,7 +157,16 @@ void loop ()
         {
             digitalWrite(BOX2LED, HIGH);
             LightBox(BOX2START, cWhite);
-            strip.setPixelColor(BOX2START+LEDPROBOX-1-i%LEDPROBOX,cOrange);
+            hsv OrangeHsv1 = OrangeHsv;
+            OrangeHsv1.s = OrangeHsv.s - Brightness2[j%BSTEPS]*(OrangeS-WhiteS)/100.0;
+            OrangeHsv1.v = OrangeHsv.v - Brightness2[j%BSTEPS]*(OrangeV-WhiteV)/100.0;
+            rgb OrangeRgb1 = hsv2rgb(OrangeHsv1);
+            strip.setPixelColor(BOX2START+LEDPROBOX-1-i%LEDPROBOX,OrangeRgb1.r*255,OrangeRgb1.g*255,OrangeRgb1.b*255);
+            hsv OrangeHsv2 = OrangeHsv;
+            OrangeHsv2.s = OrangeHsv.s - Brightness[j%BSTEPS]*(OrangeS-WhiteS)/100.0;
+            OrangeHsv2.v = OrangeHsv.v - Brightness[j%BSTEPS]*(OrangeV-WhiteV)/100.0;
+            rgb OrangeRgb2 = hsv2rgb(OrangeHsv2);
+            strip.setPixelColor(BOX2START+LEDPROBOX-1-i%LEDPROBOX+1,OrangeRgb2.r*255,OrangeRgb2.g*255,OrangeRgb2.b*255);
         }
         else
         {
@@ -102,7 +177,17 @@ void loop ()
         {
             digitalWrite(BOX3LED, HIGH);
             LightBox(BOX3START, cWhite);
-            strip.setPixelColor(BOX3START+i%LEDPROBOX,cYellow);
+            /* strip.setPixelColor(BOX3START+i%LEDPROBOX,strip.Color(YellowR, YellowG, YellowB)); */
+            hsv YellowHsv1 = YellowHsv;
+            YellowHsv1.s = YellowHsv.s - Brightness2[j%BSTEPS]*(YellowS-WhiteS)/100.0;
+            YellowHsv1.v = YellowHsv.v - Brightness2[j%BSTEPS]*(YellowV-WhiteV)/100.0;
+            rgb YellowRgb1 = hsv2rgb(YellowHsv1);
+            strip.setPixelColor(BOX3START+i%LEDPROBOX,YellowRgb1.r*255,YellowRgb1.g*255,YellowRgb1.b*255);
+            hsv YellowHsv2 = YellowHsv;
+            YellowHsv2.s = YellowHsv.s - Brightness[j%BSTEPS]*(YellowS-WhiteS)/100.0;
+            YellowHsv2.v = YellowHsv.v - Brightness[j%BSTEPS]*(YellowV-WhiteV)/100.0;
+            rgb YellowRgb2 = hsv2rgb(YellowHsv2);
+            strip.setPixelColor(BOX3START+i%LEDPROBOX-1,YellowRgb2.r*255,YellowRgb2.g*255,YellowRgb2.b*255);
         }
         else
         {
